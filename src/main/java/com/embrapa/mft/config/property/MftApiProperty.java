0c6d55fd286fe6ede7e0a14da833711880fc110d
Cleyvison Matos <cleyvisonmatos777@gmail.com>
2020-07-28 19:26:17 -0300

    TsAtual Finalizado1 28/07/2020

--- src/main/java/com/embrapa/mft/config/property/MftApiProperty.java
+++ src/main/java/com/embrapa/mft/config/property/MftApiProperty.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("mftapi")
 public class MftApiProperty {
-	private String originPermitida = "http://localhost:4200";
+	private String originPermitida = "https://frontmft.000webhostapp.com";
 	private final Seguranca seguranca = new Seguranca();
 	
 	
